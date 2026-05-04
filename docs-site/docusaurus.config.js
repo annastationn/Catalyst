491a3d94ea2882b2eaeb8c26fd6951b0ff69f9ff
annastationn <Naaaaaaaaaas@yandex.ru>
2026-05-04 13:24:58 +0300

    fix

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -20,15 +20,13 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://annastationn.github.io',
+  baseUrl: '/Catalyst/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'annastationn', // Usually your GitHub org/user name.
+  projectName: 'Catalyst', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
